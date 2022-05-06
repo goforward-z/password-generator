62afa0051f9595c6858f0e1314f1299c6fd10716
goforward-z <zlepeng.ucf@gmail.com>
2022-05-05 21:12:50 -0400

    creat start function

--- Develop/script.js
+++ Develop/script.js
@@ -15,3 +15,38 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
++
+
+//promt user for number of characters
+
+function promptCharacter(){
+  var numberOfCharacters = prompt("How Many Characters Do You want For Your Password? Pwassword Must Be in Between 8-128.")
+  if (numberOfCharacters>7 === numberOfCharacters<129){
+    return numberOfCharacters;
+  }
+  else{
+    alert("Your Password is Invalid");
+    promptCharacter();
+  }
+};
+
+//prompt the user for uppercase lowercase special characters numbers
+function promptRequrement(){
+  var promptUppercase =prompt("Do you Want Uppercase for your password?");
+  promptUppercase = promptUppercase.toLowerCase();
+  if(promptUppercase.trim()==="yes"){
+    var confirmUppercase = true;
+  }
+  else if(promptUppercase.trim()==="no"){
+    var confirmUppercase = false;
+
+  }
+}
+
+//use the answers to generator a list of possible character
+
+//use the list of possible characters to generate a random password
+
+//validate that the password meets the requrements
+
+//write the password on the screen
