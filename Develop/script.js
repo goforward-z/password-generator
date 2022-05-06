d55456d9ca3c91db22abfff0197862cc6272a357
gofoward-z <zlepeng.ucf@gmail.com>
2022-05-06 11:59:26 -0400

    add lowercase option

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,39 @@
 // Assignment code here
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialCharacter = "!@#$%^&*()+[]{}`~ ";
+var number = " 0123456789 ";
 
+var randomString ="";
+var PwasswordString ="";
+
+
+
+var generatePassword=function(){
+
+  //password length must between 8-128 characters
+  var passwordLength = parseInt(window.prompt("How Many Characters Do You want For Your Password? Pwassword Must Be in Between 8-128.")); 
+  if (passwordLength >=8 && passwordLength <= 128){
+    //choose lowerCase, UpperCase, number,special character
+    
+    //lowercase
+    var lowerCasePrompt = window.prompt("Do you want your password include lowercase letters? Yes or No.");
+    if (lowerCasePrompt==="yes" || lowerCasePrompt==="YES"){
+      randomString= randomString + lowerCase;
+      console.log(randomString);
+    }
+  }
+  
+};
+
+
+//use the answers to generator a list of possible character
+
+//use the list of possible characters to generate a random password
+
+//validate that the password meets the requrements
+
+//write the password on the screen
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,38 +49,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-+
-
-//promt user for number of characters
-
-function promptCharacter(){
-  var numberOfCharacters = prompt("How Many Characters Do You want For Your Password? Pwassword Must Be in Between 8-128.")
-  if (numberOfCharacters>7 === numberOfCharacters<129){
-    return numberOfCharacters;
-  }
-  else{
-    alert("Your Password is Invalid");
-    promptCharacter();
-  }
-};
-
-//prompt the user for uppercase lowercase special characters numbers
-function promptRequrement(){
-  var promptUppercase =prompt("Do you Want Uppercase for your password?");
-  promptUppercase = promptUppercase.toLowerCase();
-  if(promptUppercase.trim()==="yes"){
-    var confirmUppercase = true;
-  }
-  else if(promptUppercase.trim()==="no"){
-    var confirmUppercase = false;
-
-  }
-}
-
-//use the answers to generator a list of possible character
-
-//use the list of possible characters to generate a random password
-
-//validate that the password meets the requrements
-
-//write the password on the screen
